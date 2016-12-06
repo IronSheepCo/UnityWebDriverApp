b78f7a1e84913c89e271e932603cfbaa3686b45a
Radu Dita <hades2510@yahoo.com>
2016-12-06 12:48:50 +0200

    fixed text not visible for leaf children

--- client.py
+++ client.py
@@ -26,10 +26,13 @@ class TreeViewTextInput(TextInput,TreeViewNode):
         super( TreeViewTextInput, self ).__init__(**kwargs)
         self.readonly = True
         self.multiline = False
-        self.border = (0, 0, 0, 0)
         self.background_color = (0,0,0,0)
         self.height = 30
+        self.foreground_color = (1, 1, 1, 1)
+        self.color = (1, 1, 1, 1)
         self.bind( on_double_tap = self.copy_to_clipboard )
+        #hack to make the text visible
+        self.is_leaf = False
 
     def copy_to_clipboard(self, instance):
         print('copied to clipboard')
