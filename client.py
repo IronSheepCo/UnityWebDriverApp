5f5c8f50be981aab313e8e966d89feec19859ffd
Radu Dita <hades2510@yahoo.com>
2016-12-06 10:56:56 +0200

    added tree view for query results

--- client.py
+++ client.py
@@ -5,9 +5,12 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.treeview import TreeView, TreeViewLabel
 
 import requests
 import json
+
+webelement_key_id = "element-6066-11e4-a52e-4f735466cecf"
 
 class Config:
     @staticmethod
@@ -36,12 +39,21 @@ class ElementsScreen( Screen ):
         self.query_button = Button( text="Run query", height=height, size_hint=(1, None) )
         layout.add_widget( self.query_button )
         self.query_button.bind( on_press = self.run_query_callback )
+        #query label
+        query_response_label = Label( text="Response results", height=height, size_hint=(1,None) )
+        layout.add_widget( query_response_label )
+        #query response area
+        self.query_response = TreeView(height=200, size_hint=(1,None), hide_root=True)
+        layout.add_widget( self.query_response )
 
     def run_query_callback(self, instance):
         print("running xpath query "+self.xpath_query.text)
         payload = {"using":"xpath","value":self.xpath_query.text}
         xpath_req = requests.post( Config.endpoint_session("elements"), data=json.dumps(payload) )
         print(xpath_req.json())
+        for el in xpath_req.json()["data"]:
+            node = self.query_response.add_node( TreeViewLabel(text=el[webelement_key_id], is_open=True) )
+            self.query_response.add_node( TreeViewLabel(text=el["name"]) , node )
 
     def on_enter(self):
         self.label.text = "connected to "+Config.server_ip
