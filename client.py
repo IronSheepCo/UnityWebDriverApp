91dae4d0d097592a553a05eac8c050e374ab4677
Alex <alex.gal@snowglobegames.com>
2016-12-20 20:00:59 +0200

    Adding a UDP Client that waits for a certain Broadcast message on a different thread The verification if we received the message is done by pressing the Auto Connect button from the UI.

--- client.py
+++ client.py
@@ -16,9 +16,17 @@ import requests
 import json
 import sys
 
+import Queue
+import threading
+import socket
+import string
+
 from tech.ironsheep.webdriver.command import Config, Command
 
 webelement_key_id = "element-6066-11e4-a52e-4f735466cecf"
+
+UDP_BROADCAST_PORT = 23923
+UDP_LISTENING_FOR_STRING = "echo for clients"
 
 class TestCaseEntry(StackLayout, TreeViewNode):
     def __init__(self, **kwargs):
@@ -120,8 +128,27 @@ class ElementsScreen( Screen ):
 
 
 class ConnectScreen( Screen ):
-    def connect_callback(self, instance):
-        Config.server_ip = self.ids["ip"].text
+    ip = None
+
+    @staticmethod
+    def receive_ip(ip):
+        ConnectScreen.ip = ip[0]
+
+    def start_connect_callback(self, instance):
+        #check if we received a broadcast message
+        BroadCastReceiver.EventOneTimeListener.put(lambda n: ConnectScreen.receive_ip(n))
+
+        if BroadCastReceiver.broadcast_message_received == True:
+            CallbackQueue.run_callback_with_thread_nonblocking()
+            #continue with connection
+            if (ConnectScreen.ip != None):
+                self.connect_callback(instance, ConnectScreen.ip)
+
+    def connect_callback(self, instance, ip = None):
+        if (ip == None):
+            Config.server_ip = self.ids["ip"].text
+        else:    
+            Config.server_ip = ip
         print("clicked here")
         try:
             status_req = requests.get( Config.endpoint("status") )
@@ -151,11 +178,71 @@ class WebDriverApp(App):
 
         self.sm.add_widget( Builder.load_file("connect_screen.kv") )
         self.sm.add_widget( Builder.load_file("elements_screen.kv") )
+
         return self.sm
 
     def on_stop(self):
         delete_req = requests.delete(Config.endpoint_session(""))
         print("deleting session with id "+Config.session_id)
 
+
+
+class CallbackQueue():
+    callback_queue = Queue.Queue()
+
+    @staticmethod
+    def queue_callback_on_thread(func_to_call_from_main_thread):
+        CallbackQueue.callback_queue.put(func_to_call_from_main_thread)
+
+    @staticmethod
+    def run_callback_with_thread_blocking():
+        callback = CallbackQueue.callback_queue.get() #blocks until an item is available
+        callback()
+
+    @staticmethod
+    def run_callback_with_thread_nonblocking():        
+        while True:
+            try:
+                callback = CallbackQueue.callback_queue.get(False) #doesn't block
+            except Queue.Empty: #raised when queue is empty
+                break
+            callback()
+
+class BroadCastReceiver():
+    serverSock = socket
+    broadcast_message_received = False
+    EventOneTimeListener = Queue.Queue()
+
+    def Listener(self):
+        print "Starting Loop:"
+        while True:
+            data, addr = self.serverSock.recvfrom(1024)            
+            #print "Message: ", data, addr
+            if (cmp(data, UDP_LISTENING_FOR_STRING) == 0 and BroadCastReceiver.broadcast_message_received == False): #broadcast received
+                BroadCastReceiver.broadcast_message_received = True
+                CallbackQueue.queue_callback_on_thread(lambda: BroadCastReceiver.Listener_Callback(data, addr))
+    
+    @staticmethod
+    def Listener_Callback(data, addr):
+        # add code to pass data on the main Thread
+        print "Message: ", data, addr
+        BroadCastReceiver.broadcast_message_received = False
+        while True:
+            try:
+                callback = BroadCastReceiver.EventOneTimeListener.get(False)
+            except Queue.Empty: #raised when queue is empty
+                break
+            callback(addr)
+
+    def __init__(self):
+        self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.serverSock.bind(('', UDP_BROADCAST_PORT))
+        clientThread = threading.Thread(target=self.Listener).start() # start UDP listener on a new thread
+
+        #while True: #test for Thread callback
+        #    CallbackQueue.run_callback_with_thread_blocking()
+    
 if __name__ == '__main__':
+    BroadCastReceiver() #start UDP client - move it on the Connect Button ?
     WebDriverApp().run()
+
