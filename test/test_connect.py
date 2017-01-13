eb147d4b9ba9f24109f0d900d18a99629b9fccd3
Radu Dita <hades2510@yahoo.com>
2017-01-13 14:24:59 +0200

    fixed connect delete session and added test for ready

--- test/test_connect.py
+++ test/test_connect.py
@@ -11,8 +11,19 @@ class TestConnect(unittest.TestCase):
         Config.server_ip = "127.0.0.1"
 
     def test_connect(self):
-        response = requests.get( Config.endpoint("status") )
-        self.assertEqual( response.json()["ready"], True )
+        response = requests.post( Config.endpoint("session") )
+        self.assertTrue( "sessionId" in response.json() )
+        Config.session_id = response.json()["sessionId"]
 
     def tearDown(self):
-       requests.delete( Config.endpoint("session") ) 
+       requests.delete( Config.endpoint_session("") ) 
+
+
+class TestReady(unittest.TestCase):
+
+    def setUp(self):
+        Config.server_ip = "127.0.0.1"
+
+    def test_ready(self):
+        response = requests.get( Config.endpoint("status") )
+        self.assertEqual( response.json()["ready"], True )
