43c37e94fe2f1fa9121f44e758a63ce9870a2b2b
Radu Dita <hades2510@yahoo.com>
2017-01-26 16:40:01 +0200

    running test cases in background thread

--- tech/ironsheep/webdriver/testcase.py
+++ tech/ironsheep/webdriver/testcase.py
@@ -1,4 +1,5 @@
 import json
+import threading
 from command import Command
 
 class TestCaseStep:
@@ -25,6 +26,8 @@ class TestCaseStep:
 class TestCase:
     def __init__(self):
         self.steps = []
+        self.running_thread = None
+        self.test_case_callback = None
     
     def addStep(self, step):
         self.steps.append( step )
@@ -39,14 +42,51 @@ class TestCase:
     def toJson(self):
         return json.dumps( self.flatten() )
 
-    def run(self):
+    def _run_blocking(self):
         step_no = 0
+        print('starting running test case blocking')
         for step in self.steps:
             step_no = step_no + 1
-            if Command.run_command_no( step.target, step.command, step.arg ) == False:
-                step.no = step_no
-                return step
+
+            step.no = step_no
+            
+            if self.test_case_callback != None:
+                self.test_case_callback( True, step )
+
+            result = Command.run_command_no( step.target, step.command, step.arg )
+            
+            if result == False:
+                self.running_thread = None
+
+                if self.test_case_callback != None:
+                    self.test_case_callback( False, step )
+
+                self.test_case_callback = None
+
+                return False
+            else:
+                if self.test_case_callback != None:
+                    self.test_case_callback( True, step )
+
+        if self.test_case_callback != None:
+            self.test_case_callback( True, None )
+
+        self.running_thread = None
+        self.test_case_callback = None
         return True
+
+    def run(self, callback=None):
+        if callback is None:
+            print('running test case on main thread')
+            return self._run_blocking()
+        else:
+            if self.running_thread != None:
+                return 
+
+            print('running test case on background thread')
+            self.running_thread = threading.Thread(name="Thread-test case", target=self._run_blocking)
+            self.test_case_callback = callback
+            self.running_thread.start()
 
     @staticmethod
     def loadFromJson(json_string):
