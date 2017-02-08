7c27b5fd466c921a0ec609efffe30d337f1b574b
Alex <alex.gal@snowglobegames.com>
2017-02-08 18:20:52 +0200

    cleaning code

--- tech/ironsheep/webdriver/testcase.py
+++ tech/ironsheep/webdriver/testcase.py
@@ -43,35 +43,32 @@ class TestCase:
         return json.dumps( self.flatten() )
 
     def _run_blocking(self):
-        
-        step_len = len(self.steps)
-        step_no = step_len
-        print('starting running test case blocking')
+
+        step_no = len(self.steps)
+        print 'starting running test case blocking'
         for step in reversed(self.steps):
             step_no = step_no - 1
 
             step.no = step_no
-            
             if self.test_case_callback != None:
-                self.test_case_callback( True, step )
+                self.test_case_callback(True, step)
 
-            result = Command.run_command_no( step.target, step.command, step.arg )
-            
-            if result == False:
+            result = Command.run_command_no(step.target, step.command, step.arg)
+            if result is False:
                 self.running_thread = None
 
                 if self.test_case_callback != None:
-                    self.test_case_callback( False, step )
+                    self.test_case_callback(False, step)
 
                 self.test_case_callback = None
 
                 return False
             else:
                 if self.test_case_callback != None:
-                    self.test_case_callback( True, step )
+                    self.test_case_callback(True, step)
 
         if self.test_case_callback != None:
-            self.test_case_callback( True, None )
+            self.test_case_callback(True, None)
 
         self.running_thread = None
         self.test_case_callback = None
@@ -79,7 +76,7 @@ class TestCase:
 
     def run(self, callback=None):
         if callback is None:
-            print('running test case on main thread')
+            print 'running test case on main thread'
             return self._run_blocking()
         else:
             if self.running_thread != None:
