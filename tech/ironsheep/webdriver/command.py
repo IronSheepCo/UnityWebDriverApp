63a792bda310ea49d9696176ac03a2e6447af0c5
Radu Dita <hades2510@yahoo.com>
2017-02-16 14:25:36 +0200

    using non strict json parsing for all commands

--- tech/ironsheep/webdriver/command.py
+++ tech/ironsheep/webdriver/command.py
@@ -88,7 +88,7 @@ class Command:
                 pass
             return Command.wait_for_visible( xpath_query, timeout )
 
-        response = Command.run_query( xpath_query ).json()
+        response = Command.run_query( xpath_query ).json(strict=False)
 
         if "data" in response:
             if len(response["data"]) == 0:
@@ -140,8 +140,8 @@ class Command:
         endpoint = 'element/'+uuid+'/click'
         print(endpoint)
         response = requests.get( Config.endpoint_session(endpoint) )
-        print( response.json() )
-        return response.json()
+        print( response.json(strict=False) )
+        return response.json(strict=False)
    
     @staticmethod
     def _check_for_data( json_data ):
@@ -155,7 +155,7 @@ class Command:
         endpoint = 'element/'+uuid+'/attribute/'+name
         print(endpoint)
         response = requests.get( Config.endpoint_session(endpoint) )
-        json_response = response.json()
+        json_response = response.json(strict=False)
         print( json_response )
         return Command._check_for_data( json_response )
 
@@ -165,16 +165,16 @@ class Command:
         print(endpoint)
         payload = {'text':keys}
         response = requests.post( Config.endpoint_session(endpoint), json=payload )
-        print( response.json() )
-        return response.json()
+        print( response.json(strict=False) )
+        return response.json(strict=False)
     
     @staticmethod
     def name(uuid):
         endpoint = 'element/'+uuid+'/name'
         print(endpoint)
         response = requests.get( Config.endpoint_session(endpoint) )
-        print( response.json() )
-        return Command._check_for_data( response.json() )
+        print( response.json(strict=False) )
+        return Command._check_for_data( response.json(strict=False) )
 
     @staticmethod
     def timeouts( implicit=-1, page_load=-1, script=-1):
@@ -190,7 +190,7 @@ class Command:
         
         response = requests.post( Config.endpoint_session(endpoint), json=payload)
 
-        if 'error' in response.json():
+        if 'error' in response.json(strict=False):
             return False
         else:
             return True
@@ -198,7 +198,7 @@ class Command:
     @staticmethod
     def wait_for_element(xpath, timeout = 30):
         Command.timeouts( implicit=timeout*1000 )
-        response = Command.run_query( xpath ).json()
+        response = Command.run_query( xpath ).json(strict=False)
         if "data" in response:
             el = response["data"][0]
             uuid = el[webelement_key_id]
@@ -230,7 +230,7 @@ class Command:
     def is_visible(uuid):
         endpoint = 'element/'+uuid+'/visible'
         response = requests.get( Config.endpoint_session(endpoint) )
-        return Command._check_for_data( response.json() )
+        return Command._check_for_data( response.json(strict=False) )
 
     @staticmethod
     def assert_text(uuid, text_to_check):
@@ -246,7 +246,7 @@ class Command:
     def wait_for_visible(xpath, timeout = 30):
         now = time.time()
         while time.time() - now < timeout:
-            response = Command.run_query( xpath ).json()
+            response = Command.run_query( xpath ).json(strict=False)
 
             if "data" in response:
                 if len(response["data"]) == 0:
@@ -270,6 +270,6 @@ class Command:
     def highlight(uuid):
         endpoint = 'element/'+uuid+'/highlight'
         response = requests.get( Config.endpoint_session(endpoint) )
-        print( response.json() )
-        return response.json()
+        print( response.json(strict=False) )
+        return response.json(strict=False)
     
