21b4d46a031fd03de908f569bfe183b3f8ef6d63
Alex <alex.gal@snowglobegames.com>
2017-03-13 18:27:57 +0200

    Change SaveDialog to modify initial path of the popup.

--- tech/ironsheep/webdriver/dialog.py
+++ tech/ironsheep/webdriver/dialog.py
@@ -20,3 +20,11 @@ class SaveDialog(FloatLayout):
     save = ObjectProperty(None)
     text_input = ObjectProperty(None)
     cancel = ObjectProperty(None)
+    filechooser = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(SaveDialog, self).__init__(**kwargs)
+        self.PathToLoad(Command.appDir)
+
+    def PathToLoad(self, path):
+        self.filechooser.path = path
