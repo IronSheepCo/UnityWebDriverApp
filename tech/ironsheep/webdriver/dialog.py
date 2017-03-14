5d6c90704a560e810d3f55571f0953a186601798
Alex <alex.gal@snowglobegames.com>
2017-03-14 18:17:21 +0200

    SaveDialog and LoadDialog now have custom file filters The filter will be assigned in constructor under parameter:  fileFilter

--- tech/ironsheep/webdriver/dialog.py
+++ tech/ironsheep/webdriver/dialog.py
@@ -7,24 +7,34 @@ from tech.ironsheep.webdriver.command import Command
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
-    filechooser = ObjectProperty(None)
+    filechooser = ObjectProperty(None)    
 
     def __init__(self, **kwargs):
         super(LoadDialog, self).__init__(**kwargs)
         self.PathToLoad(Command.appDir)
+        if "fileFilter" in kwargs:
+            self.ChangeFileFilter(kwargs['fileFilter'])
 
     def PathToLoad(self, path):
         self.filechooser.path = path
+    
+    def ChangeFileFilter(self, val):
+        self.filechooser.filters = val
         
 class SaveDialog(FloatLayout):
     save = ObjectProperty(None)
     text_input = ObjectProperty(None)
     cancel = ObjectProperty(None)
-    filechooser = ObjectProperty(None)
+    filechooser = ObjectProperty(None)    
 
     def __init__(self, **kwargs):
         super(SaveDialog, self).__init__(**kwargs)
         self.PathToLoad(Command.appDir)
+        if "fileFilter" in kwargs:
+            self.ChangeFileFilter(kwargs['fileFilter'])
 
     def PathToLoad(self, path):
         self.filechooser.path = path
+
+    def ChangeFileFilter(self, val):
+        self.filechooser.filters = val
