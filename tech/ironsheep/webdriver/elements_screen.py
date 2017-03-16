fff572b625c7919af09d9a5d4ed3de05abc0bdb2
Alex <alex.gal@snowglobegames.com>
2017-03-16 19:13:06 +0200

    Add methods to elements_screen.py for: - showing the Test Suite Screen - add the current test to the current Test Suite.

--- tech/ironsheep/webdriver/elements_screen.py
+++ tech/ironsheep/webdriver/elements_screen.py
@@ -1,7 +1,7 @@
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, SlideTransition
 from kivy.uix.treeview import TreeView
 from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty
@@ -71,33 +71,61 @@ class ElementsScreen( Screen ):
                 if key in ['name', webelement_key_id]:
                     continue
                 self.query_response.add_node( TreeViewTextInput(text="%s : %s"%(key, el[key]), multiline=False), node)
-            
+
     def run_clear_query_callback(self, instance):
         print "Clear Query"
         self.xpath_query.text = "//uibutton"
-        
+
     def disconnect_from_app(self, instance):
         self.test_case_view.clear()
-        
+
         print "Try to disconnect"
         #Config.server_ip = "127.0.0.1"
-        
+
         try:
-            print("Deleting current session")
+            print "Deleting current session"
             delete_session_req = requests.delete(Config.endpoint_session(""))
-            print( delete_session_req.json() )
-              
+            print delete_session_req.json()
+
         except Exception:
-            popup = Popup( title="Error", content=Label(text="Could not delete previous session"), size_hint=(None,None), size=(300,200) )
+            popup = Popup(title="Error", content=Label(text="Could not delete previous session"),
+                          size_hint=(None, None), size=(300, 200))
             popup.open()
-        
+
         #slide in the connect interface
         self.manager.current = 'connect'
 
     def on_enter(self):
         self.label.text = "connected to "+Config.server_ip
-        self.query_response.bind(minimum_height=self.query_response.setter('height') )
+        self.query_response.bind(minimum_height=self.query_response.setter('height'))
 
-    def show_test_list(self):       
-
+    def show_test_suite(self):
+        '''
+        switch to test suite screen
+        '''
+        self.manager.transition = SlideTransition(direction='left')
         self.manager.current = 'test_list'
+
+    def add_to_suite(self):
+        '''
+        add current test case to current suite.
+        If the current test case was not save we'll show a popup 
+        and not insert the test case to suite.
+        '''
+        if self.test_case_view.testCaseSaved is False:
+            popup = Popup(title="Warning",
+                          content=Label(text="Current test case was not saved. \n Please save before adding to test suite.",
+                                        halign='center'),
+                          size_hint=(None, None),
+                          size=(350, 200))
+            popup.open()
+        else:
+            #add test case to current suite
+            if self.manager.has_screen('test_list'):
+                suite_screen = Screen()
+                suite_screen = self.manager.get_screen('test_list')
+                step = suite_screen.test_case_list_view.add_test_suite_step()
+
+                index = suite_screen.test_case_list_view.test_case_list.steps.index(step)
+                suite_screen.test_case_list_view.test_case_list_stack.children[index].target_input.text = self.test_case_view.test_case_path
+
